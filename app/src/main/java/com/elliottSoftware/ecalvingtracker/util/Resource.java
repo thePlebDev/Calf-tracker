f5a3b33c0a3de41efcd60f5c91da7f7d8f63d4ae
Tristan Elliott <>
2021-10-28 16:04:25 -0300

    removed unwanted code

--- app/src/main/java/com/elliottSoftware/ecalvingtracker/util/Resource.java
+++ app/src/main/java/com/elliottSoftware/ecalvingtracker/util/Resource.java
@@ -4,37 +4,32 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class Resource<T>{
-    @NonNull private final Status status;
     @NonNull private final T data;
     @NonNull private final String message;
 
-    public Resource(@NonNull Status status, @Nullable T data, @Nullable String message){
-        this.status = status;
+    public Resource( @Nullable T data, @Nullable String message){
         this.data = data;
         this.message = message;
     }
 
     public static <T> Resource<T> success(@NonNull T data, @NonNull String message){
-        return new Resource<>(Status.SUCCESS,data,message);
+        return new Resource<>(data,message);
     }
 
-    public static <T> Resource<T> loading(@Nullable T data){
-        return new Resource<>(Status.LOADING,data,null);
+    public static <T> Resource<T> loading(@Nullable T data,String message){
+        return new Resource<>(data,message);
     }
-    
+
     public static <T> Resource<T> error(@NonNull T data, @NonNull String message){
-        return new Resource<>(Status.ERROR,data,message);
+        return new Resource<>(data,message);
     }
 
 
 
-    public enum Status {SUCCESS,ERROR,LOADING}
+
 
 
     //GETTERS
-    public Status getStatus(){
-        return this.status;
-    }
     public T getData(){
         return this.data;
     }
